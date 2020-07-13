9ee8b82c29a217aebae55c387c0cbab4b53f6474
liuqing <joeyliu@gmail.com>
2020-07-13 16:35:51 -0600

    Add Buffer operator

---
+++ src/operators/buffer.go
@@ -0,0 +1,58 @@
+package operators
+
+import (
+	"github.com/amao/toy-rxgo/src/base"
+)
+
+type bufferSubscriber struct {
+	*base.OuterSubscriber
+	buffer []interface{}
+}
+
+func newBufferSubscriber(destination base.SubscriberLike, closingNotifier base.Subscribable) bufferSubscriber {
+	newInstance := new(bufferSubscriber)
+
+	selfSubscriber := base.NewSubscriber(destination)
+	selfOuterSubscriber := new(base.OuterSubscriber)
+	newInstance.OuterSubscriber = selfOuterSubscriber
+	newInstance.Subscriber = &selfSubscriber
+	newInstance.Destination.Add(newInstance)
+
+	newInstance.SetInnerNext(func(value interface{}) {
+		newInstance.buffer = append(newInstance.buffer, value)
+	})
+	innerSubscriber := base.NewInnerSubscriber(newInstance)
+	newInstance.Add(closingNotifier.Subscribe(innerSubscriber))
+
+	return *newInstance
+}
+
+func (b *bufferSubscriber) NotifyNext(innerValue interface{}) {
+	buffer := b.buffer
+	b.buffer = nil
+	b.Destination.Next(buffer)
+
+}
+
+type bufferOperator struct {
+	closingNotifier *base.Observable
+}
+
+func newBufferOperator(closingNotifier base.Observable) bufferOperator {
+	newInstance := new(bufferOperator)
+	newInstance.closingNotifier = &closingNotifier
+	return *newInstance
+}
+
+func (b *bufferOperator) Call(subscriber base.SubscriberLike, source base.Subscribable) base.SubscriptionLike {
+	nbs := newBufferSubscriber(subscriber, b.closingNotifier)
+	return source.Subscribe(nbs)
+}
+
+func Buffer(closingNotifier base.Observable) base.OperatorFunction {
+	result := func(source base.Observable) base.Observable {
+		op := newBufferOperator(closingNotifier)
+		return source.Lift(&op)
+	}
+	return result
+}
